642b833a986c9f46367ee519108d5790ce0f14fb
hexiaohong2018 <185741506@qq.com>
2018-12-21 21:58:15 +0800

    新建刷新demo

---
+++ pages/index/index.js
@@ -0,0 +1,80 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  
+ 
+  onLoad: function() {
+    // 刷新组件
+    this.refreshView = this.selectComponent("#refreshView")
+
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+
+  },
+  //触摸开始
+  handletouchstart: function (event) {
+    this.refreshView.handletouchstart(event)
+  },
+  //触摸移动
+  handletouchmove: function (event) {
+    this.refreshView.handletouchmove(event)
+  },
+  //触摸结束
+  handletouchend: function (event) {
+    this.refreshView.handletouchend(event)
+  },
+  //触摸取消
+  handletouchcancel: function (event) {
+    this.refreshView.handletouchcancel(event)
+  },
+  //页面滚动
+  onPageScroll: function (event) {
+    this.refreshView.onPageScroll(event)
+  },
+  onPullDownRefresh:function(){
+    setTimeout(() => { this.refreshView.stopPullRefresh()},5000)
+  }
+})
